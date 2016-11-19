f8358f424b06248c69cbd5098e6c85f6b55ad437
Axel Scheele <axel@Axels-MacBook-Pro.local>
2016-11-19 08:54:23 +0100

    streams implementation

--- axel_willem/overpass-stream.js
+++ axel_willem/overpass-stream.js
@@ -1,7 +1,12 @@
 var request = require('request');
+var fs = require('fs')
+
+var parse = require('./xmltransformer')
 
 var query = '(way(52.0109,4.9507,52.1510,5.2484)[highway=primary]);node(w);out;';
 
 request.post('http://overpass-api.de/api/interpreter').form({
     data: query
-}).pipe(process.stdout);
+})
+    .pipe(parse())
+    .pipe(fs.createWriteStream('osm_high.xml'));
